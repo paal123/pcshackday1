b49a08ae7aff15add9e1f6fe7fef7f40f38a6e8a
Kjetil Falnes <KFALN@equinor.com>
2019-11-29 12:36:14 +0100

    Robot controller

---
+++ Controllers/RobotController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PcsHackday1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RobotController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Sandnesforfa1";
+        }
+
+        // POST: api/Robot
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+    }
+}
